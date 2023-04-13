ecff92c8857bb6f38c271a6c9c2921b25a2acac4
Klauß <kipatok@bk.ru>
2023-04-13 17:31:32 +0300

    2.13 done

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -70,7 +70,7 @@ console.log('persons', persons)
     })
    
     
-    setPersons(persons.concat(nameObject))
+    //setPersons(persons.concat(nameObject))
     setNewName('')
     setNewNumber('')
     setFilter('')
